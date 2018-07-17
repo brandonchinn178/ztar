b33b287db967a62c4185950cd63cb47e087b1d4b
Brandon Chinn <brandon@leapyear.io>
2018-07-17 16:00:43 -0700

    Update documentation of path types

--- src/Codec/Archive/ZTar/Path.hs
+++ src/Codec/Archive/ZTar/Path.hs
@@ -5,6 +5,8 @@ Stability   :  experimental
 Portability :  portable
 
 Types for representing filepaths.
+
+The types in this file are different depending on whether the @typed-paths@ flag was enabled.
 -}
 {-# LANGUAGE CPP #-}
 
@@ -18,19 +20,37 @@ module Codec.Archive.ZTar.Path
 
 import Path
 
+-- | The type for a path to a file.
+--
+-- With untyped paths, this becomes @FilePath@.
 type PathFile b = Path b File
+
+-- | The type for a path to a directory.
+--
+-- With untyped paths, this becomes @FilePath@.
 type PathDir b = Path b Dir
 
--- | Convert a Path into a FilePath.
+-- | Convert a path into a FilePath.
+--
+-- With untyped paths, this becomes @FilePath -> FilePath@
 toFP :: Path b t -> FilePath
 toFP = toFilePath
 
 #else
 
+-- | The type for a path to a file.
+--
+-- With typed paths, this becomes @Path b File@.
 type PathFile b = FilePath
+
+-- | The type for a path to a directory.
+--
+-- With typed paths, this becomes @Path b Dir@.
 type PathDir b = FilePath
 
--- | Convert a FilePath into a FilePath.
+-- | Convert a path into a FilePath.
+--
+-- With typed paths, this becomes @Path b t -> FilePath@
 toFP :: FilePath -> FilePath
 toFP = id
 
