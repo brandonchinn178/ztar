35cb4d5fec3af756277638470c316181994e5b04
Brandon Chinn <brandon@leapyear.io>
2020-05-03 13:34:06 -0700

    Install ncurses

--- scripts/install-system-deps.sh
+++ scripts/install-system-deps.sh
@@ -11,7 +11,7 @@ function is_command() {
 function install_linux() {
     if is_command yum; then
         yum update -y --exclude=filesystem
-        yum install -y bzip2-devel zlib-devel
+        yum install -y bzip2-devel zlib-devel ncurses-devel
     fi
 }
 
